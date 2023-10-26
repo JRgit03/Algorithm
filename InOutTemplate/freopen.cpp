1fd302272e8ff6891dd37d22aad064a41251e113
JRgit03 <3411770569@qq.com>
2023-10-26 12:49:55 +0800

    Create freopen.cpp

---
+++ InOutTemplate/freopen.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define IOS ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define dbug(x) cout << #x << '=' << x << endl
+#define int long long
+#define x first
+#define y second
+#define ONLINE_JUDGE  
+using namespace std;
+const int N = 1e5 + 10, Mod = 1e9 + 7, INF = 0x3f3f3f3f;
+typedef pair<int,int> PII;
+typedef long long ll;
+
+void solve(){
+    
+}
+
+signed main(){
+    #ifndef ONLINE_JUDGE
+        freopen("E:/in.txt","r",stdin);
+        freopen("E:/out.txt","w",stdout);
+    #endif
+    IOS;
+    int t;cin>>t;
+    while(t--)solve();
+    return 0;
+}
