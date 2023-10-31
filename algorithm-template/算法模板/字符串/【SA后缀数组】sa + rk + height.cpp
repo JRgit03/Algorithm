6544578f1bda738bd3cdd39efdb0451f449b8b4b
JRgit03 <3411770569@qq.com>
2023-10-31 17:47:14 +0800

    Update 【SA后缀数组】sa + rk + height.cpp

--- algorithm-template/算法模板/字符串/【SA后缀数组】sa + rk + height.cpp
+++ algorithm-template/算法模板/字符串/【SA后缀数组】sa + rk + height.cpp
@@ -35,6 +35,32 @@ void calc_sa(){
     }
 }
 
+int cnt[N],t1[N],t2[N];
+
+void calc_sa2(){
+    int m = 127;
+    int i,*x = t1, *y = t2;
+    for(int i=0;i<m;i++) cnt[i] = 0;
+    for(int i=0;i<n;i++) cnt[x[i] = s[i]]++;
+    for(int i=0;i<m;i++) cnt[i] += cnt[i-1];
+    for(int i=n-1;i>=0;i--) sa[--cnt[x[i]]] = i;
+    for(int k=1;k<=n;k=k*2){
+        int p = 0;
+        for(int i=n-k;i<n;i++) y[p++] = i;
+        for(int i=0;i<n;i++) if(sa[i] >= k) y[p++] = sa[i] - k;
+        for(int i=0;i<m;i++) cnt[i] = 0;
+        for(int i=0;i<n;i++) cnt[x[y[i]]]++;
+        for(int i=0;i<m;i++) cnt[i] += cnt[i-1];
+        for(int i=n-1;i>=0;i--) sa[--cnt[x[y[i]]]] = y[i];
+        swap(x,y);
+        p = 1; x[sa[0]] = 0;
+        for(int i=1;i<n;i++) 
+            x[sa[i]] = y[sa[i-1]] == y[sa[i]] && y[sa[i-1] + k] == y[sa[i] + k] ? p-1 : p++;
+        if(p>=n) break;
+        m = p;
+    }
+}  
+
 void getheight(int n){
     int i,j,k=0;
     for(i=0;i<n;i++) rk[sa[i]] = i;
