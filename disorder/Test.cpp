8315c66de569d000fc1eda3b0b5659a709c6e596
JRgit03 <3411770569@qq.com>
2023-11-07 18:26:48 +0800

    Update Test.cpp

--- disorder/Test.cpp
+++ disorder/Test.cpp
@@ -1,20 +1,11 @@
 #include <bits/stdc++.h>
-#define IOS ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-// #define ONLINE_JUDGE  
 using namespace std;
-typedef long long ll;
-
-void solve(){
-    
-}
 
 int main(){
-    #ifndef ONLINE_JUDGE
-        freopen("E:/in.txt","r",stdin);
-        freopen("E:/out.txt","w",stdout);
-    #endif
-    IOS;
-    int a,b;
-    while(cin>>a>>b) cout<<a+b<<"\n";
+    int n; cin>>n;
+    vector<int> vec(n);
+    for(int i=1;i<=n;i++) vec.push_back(i);
+    cout << vec.size() << "\n";
+    for(auto x : vec) cout << x << " ";
     return 0;
 }
